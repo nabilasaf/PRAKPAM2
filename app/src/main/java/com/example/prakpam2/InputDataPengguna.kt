d7b3f0e6b0ea96a173614d45edb3cc1dc92abc04
nabilasaf <nabilasafitri861@gmail.com>
2025-10-30 22:44:51 +0700

    Menambahkan layout untuk box pengisi form daftar

--- app/src/main/java/com/example/prakpam2/InputDataPengguna.kt
+++ app/src/main/java/com/example/prakpam2/InputDataPengguna.kt
@@ -13,12 +13,14 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Shapes
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -27,7 +29,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Outline
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -60,14 +64,19 @@ fun FormPendaftaran (modifier: Modifier){
             .padding(top = 70.dp)
         ) {
             Text(text = stringResource(id= R.string.pendaftaran),
-                fontSize = 30.sp,
+                fontSize = 35.sp,
                 fontWeight = FontWeight.Bold,
             )
 
         }
         Spacer(modifier= Modifier.height(height = 15.dp))
 
-        Column (){  }
+        Column (modifier = Modifier
+            .fillMaxSize()
+            .padding(all = 20.dp)
+            .clip(shape = RoundedCornerShape(size = 20.dp))
+            .background(color = Color.DarkGray)
+            ){  }
 
     }
 
