16b0d211ce8cbf54f772c1a28d036a87f33c9e22
nabilasaf <nabilasafitri861@gmail.com>
2025-10-30 20:56:48 +0700

    Menambahkan var dan val text

--- app/src/main/java/com/example/prakpam2/InputDataPengguna.kt
+++ app/src/main/java/com/example/prakpam2/InputDataPengguna.kt
@@ -1,6 +1,24 @@
 package com.example.prakpam2
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 
 @Composable
-fun FormPendaftaran (){}
+fun FormPendaftaran (modifier: Modifier){
+    var textNama by remember { mutableStateOf(value = " ") }
+    var textAlamat by remember { mutableStateOf(value = " ") }
+    var textJK by remember { mutableStateOf(value = " ") }
+    var textSP by remember {mutableStateOf(value = " ")  }
+
+    var nama by remember { mutableStateOf(value = " ") }
+    var alamat by remember { mutableStateOf(value = " ") }
+    var jenis by remember { mutableStateOf(value = " ") }
+    var StatusPerkawinan by remember { mutableStateOf(value = " ") }
+
+    val gender:List<String> = listOf("Laki-laki","Perempuan")
+    val status: List<String> = listOf("Janda","Lajang","Duda","Menikah")
+}
